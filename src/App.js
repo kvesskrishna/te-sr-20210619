261919ed93ee89d14b83ca824e6eb7d928ecaa3d
Krishna KVESS <krishna.kkarthi@gmail.com>
2021-06-19 12:41:44 +0530

    day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// export function MyApp() {
+//   // name export
+//   // functional component
+//   return (
+//     <div className="App">
+//       <h1>App Component</h1>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+// export function Title(props) {
+//   return (
+//     <div>
+//       <h2>My Title</h2>
+//     </div>
+//   );
+// }
+
+// function Index() {
+//   return (
+//     <React.Fragment>
+//       <h1>Index Component</h1>
+//       <Title />
+//       <Title />
+//       <Title />
+//       <p>
+//         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+//         voluptatum quasi et, magni delectus voluptatem reprehenderit, quas
+//         officiis consequatur vitae, hic iure. Cumque, nobis magnam eum iure
+//         consequuntur labore quae.
+//       </p>
+//     </React.Fragment>
+//   );
+// }
+
+// export default Index;
+
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  title = "Expense Tracker";
+  style = { color: "blue", textAlign: "center" };
+  render() {
+    return (
+      <>
+        <Navbar />
+        <h1 style={this.style}>{this.title}</h1>
+        <p style={{ color: "green", textAlign: "center" }}>
+          This is a simple expense tracker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
